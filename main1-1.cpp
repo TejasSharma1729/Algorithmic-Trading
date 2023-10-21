19d42dddefc1e6cc78c9a95791b9de2ba581af45
Tejas Sharma <134224538+TejasSharma1729@users.noreply.github.com>
2023-10-21 09:53:29 +0000

    Create main1-1.cpp

---
+++ main1-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+    string line;
+    while (getline(cin, line))
+    {
+        printf("%s\n", "No Trade");
+    }
+}
