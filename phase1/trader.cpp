594527f1e076a0dd98639b8f04bfde0019fc0f7f
cse <@cse.iitb.ac.in>
2023-10-22 00:50:22 +0530

    Finished phase1:part1 with working string for reading.

--- phase1/trader.cpp
+++ phase1/trader.cpp
@@ -1,10 +1,64 @@
 #include "receiver.h"
+#include <vector>
+using namespace std;
 
-int main() {
+struct stock {
+	string name = "";
+	int price = 0;
+};
 
-    Receiver rcv;
-    sleep(5);
-    std::string message = rcv.readIML();
-    std::cout << message;
-    return 0;
+int main(int argc, char* argv[]) {
+	Receiver rcv;
+	sleep(5);
+	string message;
+	message = rcv.readIML();
+	for (int i = 0; i < message.length(); i++) if (message[i] < 32) message[i] = '\n';
+
+	if (argv[1][0] == '1') {
+		string stockName;
+		int price;
+		char bs;
+		vector<stock> stocks;
+		int i = 0;
+
+		while (i < message.length()) {
+    	    stockName = ""; 
+    	    while (message[i] != ' ') stockName += message[i++];
+    	    i++;
+    	    price = 0;
+    	    while (message[i] != ' ') price = 10*price + (int)message[i++] - 48; 
+    	    bs = message[++i];
+			while (message[i] != '\n' && i != message.length()) i++; i++;
+    	    int n = stocks.size();
+
+    	    for (int j = 0; j < stocks.size(); j++) {
+    	        if (stocks[j].name == stockName) {
+    	            if (stocks[j].price < price && bs == 'b') {
+    	                stocks[j].price = price;
+    	                cout << stockName << " " << price << " s\n";
+    	            }   
+    	            else if (stocks[j].price > price && bs == 's') {
+    	                stocks[j].price = price;
+    	                cout << stockName << " " << price << " b\n";
+    	            }   
+    	            else {
+						cout << "No Trade\n";
+					}
+    	            n = j;
+    	            break;
+    	        }   
+    	    }   
+    	    if (n == stocks.size()) {
+    	        stocks.push_back({stockName, price});
+    	        cout << stockName << " ";
+    	        cout << price << " ";
+				if (bs == 'b') {
+					cout << "s\n";
+				}
+				else {
+					cout << "b\n";
+				}
+    	    }   
+    	}
+	}
 }
