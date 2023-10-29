c696bc5a1382dc186134b1c566d172c529f18c3d
cse <@cse.iitb.ac.in>
2023-10-29 17:11:59 +0530

    Modified main file with working part1 and part2

--- phase1/trader.cpp
+++ phase1/trader.cpp
@@ -2,12 +2,20 @@
 #include <vector>
 using namespace std;
 
+template <typename T>
+struct node {
+	T val;
+	node* next;
+};
+template <typename T>
+using ptr = node<T>*;
+
 struct stock {
 	string name = "";
 	int price = 0;
 };
 
-int stringSplit(string& message, vector<string>& splits, vector<string>& orders, int i) {
+string stringSplit(string& message, vector<string>& splits, int &i) {
 	string temp;
 	string order;
 	bool flags = 1;
@@ -25,15 +33,19 @@ int stringSplit(string& message, vector<string>& splits, vector<string>& orders,
 	}
 	if (order[order.length() - 1] == 'b') order[order.length() - 1] = 's';
 	else order[order.length() - 1] = 'b';
-	orders.push_back(order);
-	return i;
+	return order;
 }
 
 int main(int argc, char* argv[]) {
 	Receiver rcv;
 	sleep(5);
 	string message;
-	message = rcv.readIML();
+	string m = rcv.readIML();
+	while (m != "") {
+		message += m;
+		m = "";
+		m = rcv.readIML();
+	}
 	for (int i = 0; i < message.length(); i++) if (message[i] < 32) message[i] = '\n';
 
 	if (argv[1][0] == '1') {
@@ -88,75 +100,183 @@ int main(int argc, char* argv[]) {
 		// Exponential Algorithm
 		vector<stock> stocks(1, {"", 0}); // 1-indexed
 		int k = 0;
-		vector<vector<int>> people;
-		vector<string> orders;
+		ptr<vector<int>> people = nullptr;
+		int peps = 0;
+		ptr<string> orders = nullptr;
+		int FinalProfit = 0;
 
 		while (k < message.length()) {
 			vector<string> splits;
-			k = stringSplit(message, splits, orders, k);
+			string order = stringSplit(message, splits, k);
 			int n = splits.size();
-			people.push_back(vector<int>(stocks.size(), 0));
-			people.back()[0] = stoi(splits[n-2]);
+			auto T = new node<vector<int>>;
+			auto U = new node<string>;
+			T->val.resize(stocks.size(), 0);
+			T->next = people;
+			U->val = order;
+			U->next = orders;
+			people = T;
+			orders = U;
+			peps++;
+
+			people->val[0] = stoi(splits[n-2]);
 			for (int i = 0; i < n-2; i += 2) {
 				bool flags = 0;
 				for (int j = 1; j < stocks.size(); j++) if (splits[i] == stocks[j].name) {
 					flags = 1;
-					people[people.size()-1][j] = stoi(splits[i+1]);
+					people->val[j] = stoi(splits[i+1]);
 					break;
 				}
 				if (!flags) {
 					stocks.push_back({splits[i], i/2+1});
-					for (int j = 0; j < people.size(); j++) people[j].push_back(0);
-					people[people.size()-1][stocks.size()-1] = stoi(splits[i+1]);
+					T = people;
+					while (T != nullptr) { 
+						T->val.push_back(0);
+						T = T->next;
+					}
+					people->val[stocks.size()-1] = stoi(splits[i+1]);
 				}
 			}
 			if (splits[n-1][0] == 's') {
-				for (int j = 0; j < stocks.size(); j++) people[people.size()-1][j] *= -1;
+				for (int j = 0; j < stocks.size(); j++) people->val[j] *= -1;
 			}
-		}
-		int n = people.size();
-		int m = stocks.size();
-		vector<uint8_t> included(n, 0);
-		bool flags = 1;
-		vector<uint8_t> maxIncl;
-		int maxProfit = 0;
 
-		while (flags) {
-			int i = 0;
-			vector<int> resultant(m, 0);
-			while (i < n) {
-				for (int j = 0; j < m; j++) resultant[j] += included[i]*people[i][j];
-				i++;
-			}
-			bool acceptable = 1;
-			for (int j = 1; j < m; j++) if (resultant[j] != 0) acceptable = 0;
-			if (acceptable) {
-				if (resultant[0] > maxProfit) {
-					maxIncl = included;
-					maxProfit = resultant[0];
-				}
-			}
-			i--;
-			while (i >= 0 && included[i] != 0) included[i--] = 0;
-			if (i == -1) flags = 0;
-			else included[i] = 1;
-		}
+			T = people->next;
+			auto prev = people;
+			U = orders;
+			auto V = U->next;
 
-		vector<int> resultant(m, 0);
-		for (int i = 0; i < n; i++)
-		{
-			if (maxIncl[i] == 0) {cout << "No Trade\n"; continue;}
-			bool flags = 1;
-			for (int j = 1; j < m; j++) {resultant[j] += people[i][j]; if (resultant[j] != 0) flags = 0;}
-			if (flags) {
-				for (int j = i; j >= 0; j--) if (maxIncl[j] == 1) {
-					cout << orders[j] << "#\n";
-					maxIncl[j] = 0;
+			while (T != nullptr) { 
+				bool flags = 1;
+				for (int j = 1; j < stocks.size(); j++) {
+					if (T->val[j] != people->val[j]) flags = 0;
 				}
+				if (flags == 1 && T->val[0] < people->val[0]) {
+					prev->next = T->next;
+					U->next = V->next;
+					delete T;
+					delete V;
+					T = prev->next;
+					peps--;
+				}
+				else {
+					prev = T;
+					T = T->next;
+					U = V;
+				}
+				V = U->next;
 			}
-			else cout << "No Trade\n";
+			// DELETING OUTSTANDING ORDERS OF NET GAIN LESS THAN THIS BUT SAME TRADE.
+			
+			bool flags = 0;
+			int m = stocks.size();
+			vector<uint8_t> included(peps, 0); 
+			// The person new will always be there. If not then this arbitrage would have been caught earlier.
+			included[0] = 1;
+			vector<uint8_t> maxIncl(peps, 0);
+			int maxProfit = 0;
+			
+			while (flags == 0) {
+				int i = 0;
+				vector<int> resultant(m, 0);
+				T = people;
+				while (T != nullptr) {
+					for (int j = 0; j < m; j++) resultant[j] += included[i]*T->val[j];
+					T = T->next;
+					i++;
+				}
+				bool acceptable = 1;
+				for (int j = 1; j < m; j++) if (resultant[j] != 0) acceptable = 0;
+				if (acceptable) {
+					if (resultant[0] > maxProfit) {
+						maxIncl = included;
+						maxProfit = resultant[0];
+					}
+				}
+				i--;
+				while (i >= 0 && included[i] != 0) included[i--] = 0;
+				if (i == -1) flags = 1;
+				else included[i] = 1;
+			}
+			// EXPONENTIAL COMPLEXITY PART ABOVE
+
+			if (maxProfit > 0) {
+				int i = 0;
+				T = people;
+				ptr<vector<int>> prev = nullptr;
+				U = nullptr;
+				V = orders;
+
+				while (T != nullptr) {
+					if (maxIncl[i] == 1) {
+						peps--;
+						if (prev == nullptr) {
+							people = people->next;
+							orders = orders->next;
+							delete T;
+							cout << V->val << "#\n";
+							delete V;
+							T = people;
+							V = orders;
+						}
+						else {
+							U->next = V->next;
+							prev->next = T->next;
+							cout << V->val << "\n";
+							delete V;
+							delete T;
+							T = prev->next;
+							V = U->next;
+						}
+					}
+					else {
+						prev = T;
+						U = V;
+						T = T->next;
+						V = V->next;
+					}
+					i++;
+				}
+				FinalProfit += maxProfit;
+			}
+
+			else {
+				cout << "No Trade\n";
+				T = people->next;
+				U = orders;
+				V = orders->next;
+				auto prev = people;
+
+				while (T != nullptr) { 
+					bool flags = 1;
+					for (int j = 0; j < stocks.size(); j++) {
+						if (T->val[j] + people->val[j] != 0) flags = 0;
+					}
+					if (flags == 1) {
+						prev->next = T->next;
+						U->next = V->next;
+						delete T;
+						delete V;
+						prev = people;
+						U = orders;
+						orders = orders->next;
+						people = people->next;
+						delete prev;
+						delete U;
+						peps -= 2;
+						break;
+					}
+					else {
+						prev = T;
+						T = T->next;
+						U = V;
+						V = V->next;
+					}
+				}
+				// DELETING OUTSTANDING ORDERS THAT CANCEL EACH OTHER.
+			}
 		}
-		cout << maxProfit << "\n";
+		cout << FinalProfit << "\n";
 	}
 
 	if (argv[1][0] == '3') {
